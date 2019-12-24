2811cfd000ac96a2b9fbd28c886828aafbe0403a
Ubuntu <ubuntu@ip-172-31-47-171.ec2.internal>
2019-12-24 15:41:43 +0000

    import flask, pymongo

--- .~c9_invoke_6KCHkm.py
+++ .~c9_invoke_6KCHkm.py
@@ -5,10 +5,6 @@ import re
 from bson.objectid import ObjectId
 
 
-app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'DataCentricMilestone3'
-app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@cluster0-jhmsx.mongodb.net/DataCentricMilestone3?retryWrites=true"
-
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
