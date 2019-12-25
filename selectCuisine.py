502f555ca3fc04238e2c88a6356df81bc94c8665
Ubuntu <ubuntu@ip-172-31-47-171.ec2.internal>
2019-12-25 11:31:42 +0000

    import forms libraries.

---
+++ selectCuisine.py
@@ -0,0 +1,4 @@
+from flask_wtf import FlaskForm
+from wtforms import validators, ValidationError
+from wtforms.validators import DataRequired
+from wtforms.field import TextField, TextAreaField, SubmitField, FileField, SelectField
