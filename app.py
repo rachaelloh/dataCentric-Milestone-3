464924a6b41c0936c610ef5cf02bc55afe772c30
Ubuntu <ubuntu@ip-172-31-47-171.ec2.internal>
2019-12-25 11:18:03 +0000

    routing get recipes.

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_pymongo import PyMongo
 import re
 from bson.objectid import ObjectId
 
-
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'DataCentricMilestone3'
 app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@cluster0-jhmsx.mongodb.net/DataCentricMilestone3?retryWrites=true"
@@ -14,14 +13,35 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html", 
-                           recipes=mongo.db.recipes.find())
+    #return render_template("recipes.html", 
+                           #recipes=mongo.db.recipes.find())
                            
+    cuisine = None
+    cuisine_mapping = dict(it='Italian Cuisine', ch="Chinese Cuisine", jp="Japanese Cuisine", kr="Korean Cuisine")
+
+    if request.args.get('cuisine'):
+        cuisine = cuisine_mapping.get(request.args.get('cuisine'))
+
+        
+    if cuisine :
+        recipes = mongo.db.recipes.find({ '$or': dict(cuisine=cuisine) })
+         
+        # Passing result of args into filters
+        # And passing each matching _id into filters
+
+        result = []
+        for recipe in recipes:
+            recipe_ = dict(cuisine=recipe['cuisine'], recipe=recipe['recipe'], allergens=recipe['allergens'], ingredients=recipe['ingredients'],
+            methods=recipe['methods'], image=recipe.get('image'), _id=recipe['_id'])
+            result.append(recipe_)
+    else:
+        result=mongo.db.recipes.find()
+    
+    
     form = FilterForm()
     if request.method == "POST":
         return redirect(url_for('get_recipes', cuisine=form.cuisine.data))
-    return render_template("recipes.html",
-        recipes=result, form=form)                           
+    return render_template("recipes.html", recipes=result, form=form)                           
                            
                            
 
