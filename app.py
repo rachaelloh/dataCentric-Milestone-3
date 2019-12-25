c3abadaf3c2a3430495aafe8b14de977d59433c3
Ubuntu <ubuntu@ip-172-31-47-171.ec2.internal>
2019-12-25 09:36:51 +0000

    linked recipes with main html page, added header in recipes page.

--- app.py
+++ app.py
@@ -9,7 +9,16 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'DataCentricMilestone3'
 app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@cluster0-jhmsx.mongodb.net/DataCentricMilestone3?retryWrites=true"
 
+mongo = PyMongo(app)
 
+@app.route('/')
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", 
+                           recipes=mongo.db.recipes.find())
+                           
+                           
+                           
 # "magic code" -- boilerplate
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
